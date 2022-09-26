a576e60383623c22244c59b6d7718eaa97106311
Jordan Holmes <jholmes3006@gmail.com>
2022-09-26 11:00:52 +0100

    Allows cross origin resource sharing

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const {
 
 const apiRouter = require("./routes/api-router.js");
 
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
